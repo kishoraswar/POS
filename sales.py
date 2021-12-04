9b889b51123828b450e334fa370e5248231ee01a
kishor aswar <kishoraswar@gmail.com>
2021-12-04 09:50:41 +0530

    updated sales

--- sales.py
+++ sales.py
@@ -1,4 +1,6 @@
 from os import stat
+import os
+from sqlite3.dbapi2 import paramstyle 
 from tkinter import*
 from tkinter import font
 import sqlite3
@@ -13,8 +15,99 @@ class salesClass:
         self.root.title("POS | By Kishor Aswar")
         self.root.config(bg="white")
         self.root.focus_force()
+#===================variable
+        self.var_invoice=StringVar()
+        self.bill_list=[]
+        
 #=============title
         lbl_title=Label(self.root,text="View Customer Bills", font=("goudy old style",30),fg="white",bd=3,relief=RIDGE,bg="#184a45").pack(side=TOP,fill=X,padx=10,pady=20)
+        lbl_invoice=Label(self.root,text="Invoice No",font=("times new roman",15),bg="White").place(x=50,y=100)
+        txt_invoice=Entry(self.root,textvariable=self.var_invoice,font=("times new roman",15),bg="lightyellow").place(x=160,y=100,width=180)
+        
+        btn_search=Button(self.root,text="Search",command=self.search,font=("times new roman",15,"bold"),bg="#2196f3",fg="white",cursor="hand2").place(x=360,y=100,width=120,height=28)        
+        btn_clear=Button(self.root,text="Clear",command=self.clear,font=("times new roman",15,"bold"),bg="lightgray",fg="black",cursor="hand2").place(x=490,y=100,width=120,height=28)        
+        
+        #====bill list==
+
+        sales_Frame=Frame(self.root,bd=3,relief=RIDGE)
+        sales_Frame.place(x=50,y=140,width=200,height=330)
+        
+        scrolly=Scrollbar(sales_Frame,orient=VERTICAL)
+        self.Sales_List=Listbox(sales_Frame,font=("goudy old style",15),bg="white",yscrollcommand=scrolly.set)
+        scrolly.pack(side=RIGHT,fill=Y)
+        scrolly.config(command=self.Sales_List.yview)
+        self.Sales_List.pack(fill=BOTH,expand=1) 
+        self.Sales_List.bind("<ButtonRelease-1>",self.get_Data)  
+        #====bill area==
+        bill_Frame=Frame(self.root,bd=3,relief=RIDGE)
+        bill_Frame.place(x=280,y=140,width=410,height=330)
+      
+        lbl_title2=Label(bill_Frame,text="Bill Area", font=("goudy old style",20),bg="orange").pack(side=TOP,fill=X)
+
+        scrolly2=Scrollbar(bill_Frame,orient=VERTICAL)
+        self.bill_area=Text(bill_Frame,font=("goudy old style",15),bg="lightyellow",yscrollcommand=scrolly2.set)
+        scrolly2.pack(side=RIGHT,fill=Y)
+        scrolly2.config(command=self.Sales_List.yview)
+        self.bill_area.pack(fill=BOTH,expand=1)   
+
+#==========image=============
+        self.BillPhoto=Image.open("images/cat2.jpg")
+        self.BillPhoto=self.BillPhoto.resize((450,300),Image.ANTIALIAS)
+        self.BillPhoto=ImageTk.PhotoImage(self.BillPhoto)      
+
+        lbl_image=Label(self.root,bd=0,image=self.BillPhoto)
+        lbl_image.place(x=700,y=110)  
+
+        self.show()
+#=========================
+    def show(self):  
+        del self.bill_list[:]
+        self.Sales_List.delete(0,END)
+        #print(os.listdir('../pos')) bill1.txt,category.py
+        for i in os.listdir('bill'):
+            #print(i.split('.'),i.split('.')[-1])
+            if i.split('.')[-1]=='txt':
+                self.Sales_List.insert(END,i)
+                self.bill_list.append(i.split('.')[0])
+
+    def get_Data(self,ev):  #ev-event
+        index_=self.Sales_List.curselection()
+        file_name=self.Sales_List.get(index_)
+        print(file_name)
+        self.bill_area.delete('1.0',END) #clearing bill area
+        fp=open(f'bill/{file_name}','r')
+        for i in fp:
+               self.bill_area.insert(END,i)
+        fp.close()
+
+
+#==============search invoice
+    def search(self):
+        if self.var_invoice.get()=="":
+            messagebox.showerror("Error","Invoice No.Should Be Required",parent=self.root)
+        else:
+            #print(self.bill_list,self.var_invoice)
+            if self.var_invoice.get() in self.bill_list:
+                #print("yes find the invoice")
+                fp=open(f'bill/{self.var_invoice.get()}.txt','r')
+                self.bill_area.delete('1.0',END)
+                for i in fp:
+                   self.bill_area.insert(END,i)
+                fp.close()
+            else:
+                messagebox.showerror("Error","Invalid Invoice Number!",parent=self.root)
+ 
+ #==============Clear 
+    def clear(self):
+         self.show()
+         self.bill_area.delete('1.0',END)
+         self.var_invoice.set("")
+
+
+
+        
+
+
 
 
 
